2494beaee80f0ca57754f86215ec031d05c3280e
Ge Ou <ougejiajia@gmail.com>
2021-04-19 11:52:44 -0700

    deleted test

--- readerclub/src/main/java/com/team6/readerclub/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.team6.readerclub;
-
-import com.team6.readerclub.Role;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RoleRepository extends MongoRepository<Role, String> {
-    Role findByRole(String role);
-}
